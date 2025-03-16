56627d90ab84b96895166e286715287ca686e1f7
SANGSU0203 <starcarry0203@naver.com>
2025-03-16 12:24:52 +0900

    휴대폰 인증 버튼 추가

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,35 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header.jsx";
-import Footer from '../components/Footer.jsx'
+import Footer from '../components/Footer.jsx';
 import "../css/Login.css";
 import logo from "../image/logo/spLogo.png";
+import phoneIcon from "../image/icon/mobile-phone.png"; 
 
 function Login() {
+  const [showPhoneAuth, setShowPhoneAuth] = useState(false);
+
   return (
     <div>
-    <div className="header">
+      <div className="header">
         <Header />
-    </div>
+      </div>
 
-        <div className="body sideMargin">
-            <div className="login-container">
-              {/* 좌측 영역 */}
-              <div className="login-left">
-                <img src={logo} alt="로고" className="logo" />
-                <h2>"환경을 살리는 알뜰 쇼핑 플랫폼"</h2>
-                <br></br>
-                <h2>세일로문에 가입하실 방법을</h2>
-                <h2>선택해주세요</h2>
-                <br></br>
-                <h4>유통기한 임박 상품 구매가</h4>
-                <h4>환경 보호와 연결된다는 점</h4>
+      <div className="body sideMargin">
+        <div className="login-container">
+          {/* 좌측 영역 */}
+          <div className="login-left">
+            <img src={logo} alt="로고" className="logo" />
+            <h2>"환경을 살리는 알뜰 쇼핑 플랫폼"</h2>
+            <br></br>
+            <h2>세일로문에 가입하실 방법을</h2>
+            <h2>선택해주세요</h2>
+            <br></br>
+            <h4>유통기한 임박 상품 구매가</h4>
+            <h4>환경 보호와 연결된다는 점</h4>
+          </div>
+
+          {/* 우측 로그인 영역 / 회원가입 클릭 시 휴대폰 인증으로 변경 */}
+          <div className="login-right">
+            {showPhoneAuth ? (
+              <div className="phone-auth-container">
+                <button className="phone-auth-button">
+                  <img src={phoneIcon} alt="휴대폰 인증 아이콘" className="phone-auth-icon" />
+                  <span>휴대폰 인증</span>
+                </button>
               </div>
-
-
-              {/* 우측 로그인 영역 */}
-              <div className="login-right">
-                <div className="main-login">로그인
-                </div>
+            ) : (
+              <div>
+                <div className="main-login">로그인</div>
                 <form className="login-form">
                   <input type="text" placeholder="아이디" />
                   <input type="password" placeholder="비밀번호" />
@@ -40,13 +50,11 @@ function Login() {
                     <div className="links">
                       <a href="#">아이디 찾기</a> | 
                       <a href="#">비밀번호 재설정</a> |
-                      <a href="#">회원가입</a>
+                      <a href="#" onClick={(e) => { e.preventDefault(); setShowPhoneAuth(true); }}>회원가입</a>
                     </div>
                   </div>
-                  <button type="submit" className="login-btn">
-                    로그인
-                  </button>
-                  <hr class="divider"></hr>
+                  <button type="submit" className="login-btn">로그인</button>
+                  <hr className="divider"></hr>
                 </form>
                 <div className="social-login">
                   <p>간편하게 로그인</p>
@@ -57,15 +65,15 @@ function Login() {
                   </div>
                 </div>
               </div>
-            </div>
+            )}
+          </div>
         </div>
+      </div>
 
-
-    <div className="footer">
-                <Footer />
+      <div className="footer">
+        <Footer />
+      </div>
     </div>
-    </div>
-
   );
 }
 
