9e72b146918fb95d7612bdc0cecc3ee60d7bfb15
SANGSU0203 <starcarry0203@naver.com>
2025-03-17 16:53:56 +0900

    소비자 회원가입 페이지

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -27,7 +27,7 @@ function Register() {
 
             <div class="label-group">
                 <label id="password-label">패스워드<span className="required">*</span></label>
-                <label id="password-confirm-label">패스워드 확인</label>
+                <label id="password-confirm-label">패스워드 확인<span className="required">*</span></label>
             </div>
 
           <div class="input-row">
@@ -44,8 +44,82 @@ function Register() {
             <button id="nickname-check-btn">닉네임 중복체크</button>
             </div>
           <p id="nickname-info">*게시글 작성시 사용할 닉네임을 입력해주세요</p>
-      </div>
 
+          <label id="name-label">이름<span class="required">*</span></label>
+            <div class="input-name">
+                <input type="text" id="name-input" placeholder="이름을 입력해주세요" />
+            </div>
+
+            <label id="email-label">이메일<span className="required">*</span></label>
+          <div className="email-container">
+            <input type="text" id="email-input" placeholder="이메일 입력" />
+            <span>@</span>
+
+            {emailDomain === "" ? (
+              <input type="text" id="email-domain" placeholder="" />
+            ) : (
+              <input type="text" id="email-domain" value={emailDomain} readOnly />
+            )}
+
+            <select id="email-select" onChange={(e) => setEmailDomain(e.target.value)}>
+              <option value="">직접 입력</option>
+              <option value="naver.com">naver.com</option>
+              <option value="daum.net">daum.net</option>
+              <option value="kakao.com">kakao.com</option>
+              <option value="gmail.com">gmail.com</option>
+            </select>
+          </div>
+
+          <label id="phone-label">전화번호<span className="required">*</span></label>
+            <div className="phone-input">
+	            <input type="text" id="phone-input-1" placeholder="" />
+              <span>ㅡ</span>
+              <input type="text" id="phone-input-2" placeholder="" />
+              <span>ㅡ</span>
+              <input type="text" id="phone-input-3" placeholder="" />
+            </div>
+
+            <label id="address-main-label">주소<span className="required">*</span></label>
+          <div className="address-group">
+              <input type="text" id="zipcode-input" placeholder="" />
+              <button id="housecode-btn">우편번호 찾기</button>
+          </div>
+
+          <div className="address-input-group">
+              <input type="text" id="address-input" placeholder="" />
+              <input type="text" id="address-detail" placeholder="상세주소를 입력해주세요." />
+          </div>
+
+          <label id="birth-label">생년월일<span className="required">*</span></label>
+            <div className="birth-input">
+              <select id="birth-year">
+                <option>선택</option>
+                {[...Array(100)].map((_, i) => (
+                  <option key={i}>{2025 - i}</option>
+                ))}
+              </select>
+              <span className="date">년</span>
+              <select id="birth-month">
+                <option>선택</option>
+                {[...Array(12)].map((_, i) => (
+                  <option key={i + 1}>{i + 1}</option>
+                ))}
+              </select>
+              <span className="date">월</span>
+              <select id="birth-day">
+                <option>선택</option>
+                {[...Array(31)].map((_, i) => (
+                  <option key={i + 1}>{i + 1}</option>
+                ))}
+              </select>
+              <span className="date">일</span>
+            </div>
+
+            <div class="register-btn-container">
+                <button id="register-btn">회원가입</button>
+            </div>
+
+      </div>
 
       </div>
     </div>
