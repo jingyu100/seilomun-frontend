5cf735a7a248be5a624bc03a70c8e58c88724a5a
SANGSU0203 <starcarry0203@naver.com>
2025-03-19 12:33:53 +0900

    전체내용수정

--- frontend/src/pages/Business_number.jsx
+++ frontend/src/pages/Business_number.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
 import "../css/Selogin.css";
 import logo from "../image/logo/spLogo.png";
 
 function Business_number() {
-    const [state, setState] = useState(false);
     return(
-        <div className ="Business-container">
-
+        <div className = "body sideMargin">
+         <div className = "Business-container">
+            
+         </div>
         </div>
     );
 }
