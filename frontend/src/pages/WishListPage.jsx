119b2653d18a9e43942b66df4a9eba3d6f0053e4
Parkminki-0110 <yuasli0110@g.yju.ac.kr>
2025-04-01 18:00:57 +0900

    위시리스트 생성
    위시리스트 페이지, 컴포넌트 생성

--- frontend/src/pages/WishListPage.jsx
+++ frontend/src/pages/WishListPage.jsx
@@ -3,11 +3,27 @@ import "../css/frame.css";
 import Footer from "../components/Footer.jsx";
 import SideMenuBtn from "../components/sideBtn/SideMenuBtn.jsx";
 import Header from "../components/Header.jsx";
+import { useEffect } from "react";
+import WishListEmpty from "../components/WishListEmpty.jsx";
+import WishListFilled from "../components/WishListFilled.jsx";
 
 const WishListPage = () => {
+  useEffect(() => {}, []);
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-500">Tailwind CSS 적용 테스트</h1>
+    <div>
+      <div className="header">
+        <Header />
+      </div>
+
+      <div className="body sideMargin">
+        <SideMenuBtn />
+        {wishlist.length === 0 ? <WishListEmpty /> : <WishListFilled />}
+      </div>
+
+      <div className="footer">
+        <Footer />
+      </div>
     </div>
   );
 };
