17b6ac62528b9148f496de654c523b556f2f05e2
Parkminki-0110 <yuasli0110@g.yju.ac.kr>
2025-04-11 14:26:12 +0900

    로그인 수정

--- frontend/src/pages/NaverLoginCallBack.jsx
+++ frontend/src/pages/NaverLoginCallBack.jsx
@@ -6,20 +6,8 @@ const NaverLoginCallback = () => {
   const navigate = useNavigate();
   const { setIsLoggedIn } = useLogin();
   useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const accessToken = urlParams.get("accessToken");
-    const refreshToken = urlParams.get("refreshToken");
-    if (accessToken && refreshToken) {
-      // JWT를 쿠키에 저장
-      document.cookie = `Authorization=${accessToken}; Path=/; HttpOnly; Secure; SameSite=Strict`;
-      document.cookie = `RefreshToken=${refreshToken}; Path=/; HttpOnly; Secure; SameSite=Strict`;
-
-      setIsLoggedIn(true);
-      navigate("/"); // 로그인 성공 후 홈으로 이동
-    } else {
-      console.error("OAuth 로그인 실패");
-      navigate("/login?error=true");
-    }
+    setIsLoggedIn(true);
+    navigate("/"); // 로그인 성공 후 홈으로 이동
   }, [navigate, setIsLoggedIn]);
 
   return <div>로그인 중...</div>;
