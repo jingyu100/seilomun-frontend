10d2d42d7f374d12757502aa94de6cfb79c73d0c
Parkminki-0110 <yuasli0110@g.yju.ac.kr>
2025-06-02 20:26:21 +0900

    Revert "즐겨찾기 기능 미완성"
    This reverts commit 27ee068b0c4b83903e57509a9464e93af8f38cc5.

--- frontend/src/Hooks/useStoreInfo.js
+++ frontend/src/Hooks/useStoreInfo.js
@@ -3,9 +3,9 @@ import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 export default function useStoreInfo() {
-  const { sellerId } = useParams();
-  const navigate = useNavigate();
-  const [store, setStore] = useState(null);
+   const { sellerId } = useParams();
+   const navigate = useNavigate();
+   const [store, setStore] = useState(null);
 
   useEffect(() => {
     if (!sellerId) return;
@@ -27,6 +27,7 @@ export default function useStoreInfo() {
           sellerPhotoDto: null,
           sellerRegisterDto: null, // 필요한 경우 백에서 받아서 넣기
         });
+
       } catch (error) {
         console.error("API 요청 실패:", error);
         // setStore(null);
@@ -37,7 +38,7 @@ export default function useStoreInfo() {
     storeInfo();
   }, [sellerId, navigate]);
 
-  return { store, sellerId };
+  return { store };
 
-  // const { sellerRegisterDto, sellerInformationDto, sellerPhotoDto } = store;
+  const { sellerRegisterDto, sellerInformationDto, sellerPhotoDto } = store;
 }
--- frontend/src/components/Store/StoreHead.jsx
+++ frontend/src/components/Store/StoreHead.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import useStoreInfo from "../../Hooks/useStoreInfo.js";
 import Rating from "../StarRating.jsx";
 import StoreMiniInfo from "./StoreMiniInfo.jsx";
 import Inquiry from "./Inquiry.jsx";
 import StoreInfo from "./StoreInfo.jsx";
 import FavoriteButtonBox from "./FavoriteButtonBox.jsx";
 
-export default function StoreHead({ store }) {
+export default function StoreHead() {
+  const { store } = useStoreInfo();
   if (!store) return null;
 
-  // const { sellerInformationDto, sellerPhotoDto } = store;
-  const { sellerInformationDto } = store;
+  const { sellerInformationDto, sellerPhotoDto } = store;
   const sellerId = sellerInformationDto?.sellerId;
 
   return (
