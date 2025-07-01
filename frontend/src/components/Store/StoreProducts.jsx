ab18100f602c7a47b22532491b915ed7a63b1147
logic-errors <kab03050317@gmail.com>
2025-07-01 14:28:23 +0900

    css 수정 3

--- frontend/src/components/Store/StoreProducts.jsx
+++ frontend/src/components/Store/StoreProducts.jsx
@@ -21,6 +21,7 @@ export default function StoreProducts({
       className="productItem"
       style={{
         width: "200px",
+        height: "250px",
         border: "1px solid #ccc",
         borderRadius: "10px",
         padding: "15px",
@@ -29,49 +30,62 @@ export default function StoreProducts({
     >
       <Link
         to={`/sellers/${sellerId}/products/${id}`}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          flexGrow: 1,
+          height: "100%",
+        }}
       >
         <img 
           className="store-product-image"
           src={thumbnailUrl} 
           alt={name}
         />
-        <h3>{name}</h3>
+        <h3 style={{
+          maxHeight: "2.8em",
+          marginBottom: "5px",
+        }}>{name}</h3>
         <p style={{
-              fontSize: "15px",
+              fontSize: "14.5px",
               lineHeight: "1.4em",
-              maxHeight: "2.8em",
+              maxHeight: "1.4em",
               overflow: "hidden",
               whiteSpace: "normal",
-              display: "block"
+              display: "block",
+              marginBottom: "7px",
             }}>{description}</p>
         <div style={{
-          display: "flex",
-          gap: "5px",
-
+          marginTop: "auto",
         }}>
-          <p>
-            <strong style={{
-              fontSize: "17px",
-              color: "red",
-            }}>{discountPrice.toLocaleString()}원</strong>
-          </p>
           <p style={{
-              fontSize: "14.5px",
-              color: "gray",
-              textDecoration: "line-through",
-            }}>{originalPrice.toLocaleString()}원</p>
-          <p style={{
-              fontSize: "15px",
-              color: "red",
-              
-            }}>
+            fontSize: "14px",
+            color: "gray",
+            marginBottom: "5px",
+          }}>{date}</p>
+          <div style={{
+            display: "flex",
+            gap: "5px",
+          }}>
+            <p>
+              <strong style={{
+                fontSize: "17px",
+                color: "#000",
+              }}>{discountPrice.toLocaleString()}원</strong>
+            </p>
+            <p style={{
+                fontSize: "14.5px",
+                color: "gray",
+                textDecoration: "line-through",
+              }}>{originalPrice.toLocaleString()}원</p>
+            <p style={{
+                fontSize: "15px",
+                color: "red",
+              }}>
              <strong>{currentDiscountRate}%</strong>
             </p>
+          </div>
         </div>
-        <p style={{
-              fontSize: "14px",
-              color: "gray",
-            }}>{date}</p>
       </Link>
     </div>
   );
