49422eec9736014cae1dabdf168f75b06df80721
Parkminki-0110 <yuasli0110@g.yju.ac.kr>
2025-07-10 15:10:30 +0900

    Update OrderSubmitBar.jsx

--- frontend/src/pages/payment/OrderSubmitBar.jsx
+++ frontend/src/pages/payment/OrderSubmitBar.jsx
@@ -129,7 +129,7 @@ const OrderSubmitBar = ({
         payType: "CARD",
         orderName: orderName, // 🆕 동적 주문명 사용
         yourSuccessUrl: `${API_BASE_URL}`,
-        yourFailUrl: `${API_BASE_URL}/api/orders/toss/fail`,
+        yourFailUrl: `${API_BASE_URL}`,
       };
 
       console.log("📦 최종 주문 데이터:", orderData);
