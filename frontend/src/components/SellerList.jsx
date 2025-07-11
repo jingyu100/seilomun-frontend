ac9c8b7ac74d298842c2cbb1977fa7239582d09d
logic-errors <kab03050317@gmail.com>
2025-07-11 15:55:43 +0900

    Update SellerList.jsx

--- frontend/src/components/SellerList.jsx
+++ frontend/src/components/SellerList.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useSearchParams } from "react-router-dom";
-import api, { API_BASE_URL } from "../api/config.js";
+import api, { API_BASE_URL, S3_BASE_URL } from "../api/config.js";
 import "../css/customer/SellerList.css";
 
 const SellerList = () => {
